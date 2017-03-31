7f1e099209b4a4ff63f38ef432cc08be91d0d818
Eduardo Cordeiro <eduardo.cordeiro@infnet.edu.br>
2017-03-31 17:04:21 -0300

    Ajuste de versão para último commit

--- version.php
+++ version.php
@@ -26,6 +26,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->version   = 2017032200; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2017033100; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2016051900; // Requires this Moodle version.
 $plugin->component = 'local_autocompgrade'; // Full name of the plugin (used for diagnostics).
